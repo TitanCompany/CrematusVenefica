661f930cb964d62a231daaf87fa181a765131f81
Den4ik <deniel270202@gmail.com>
2022-03-23 13:13:17 +0300

    Added damage for bullet

--- Assets/Scripts/BulletScript.cs
+++ Assets/Scripts/BulletScript.cs
@@ -6,6 +6,15 @@ public class BulletScript : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Destroy(gameObject);
+        if(collision.collider != GameObject.FindGameObjectWithTag("Player").GetComponent<BoxCollider2D>())
+        {
+            if(collision.transform.tag == "Mob")
+            {
+                var enemy = collision.gameObject.GetComponent<Enemy>();
+                enemy.TakeDamage(20);
+                Destroy(gameObject);
+            }
+            Destroy(gameObject, 2f);
+        }
     }
 }
--- Assets/Scripts/Player/PlayerShoot.cs
+++ Assets/Scripts/Player/PlayerShoot.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class PlayerShoot : MonoBehaviour
 {
     public Camera cam;
+    public Rigidbody2D rb;
 
     public Transform firePoint;
     public GameObject bulletPrefab;
+    private float angle;
 
     public float bulletForce = 20f;
 
@@ -17,6 +19,8 @@ public class PlayerShoot : MonoBehaviour
     void Update()
     {
         mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 lookDir = mousePos - rb.position;
+        angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
         if (Input.GetButtonDown("Fire1"))
         {
             Shoot();
@@ -25,9 +29,12 @@ public class PlayerShoot : MonoBehaviour
 
     private void Shoot()
     {
+        
+        firePoint.Rotate(0, 0, angle);
         GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
         Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
         rb.AddForce(firePoint.up*bulletForce, ForceMode2D.Impulse);
+        firePoint.Rotate(0, 0, -angle);
     }
 
     private void FixedUpdate()
