64692443ea7ea9b639fc37b9f5f8426f502a0272
LoXur-ux <starkaw@yandex.ru>
2022-05-05 00:58:02 +0300

    Added Pathfinder fro enemies.

---
+++ Assets/Skripts/Enemy/CatController.cs
@@ -0,0 +1,113 @@
+using Pathfinding;
+using Spine.Unity;
+using UnityEngine;
+
+public class CatController : Enemy
+{
+	public SkeletonAnimation skeletonAnimation;
+	public AnimationReferenceAsset idle, run;
+	public string currentState;
+	public float speed, dashSpeed;
+	private Rigidbody2D rigidbody;
+	public string currentAnimation;
+	private Vector3 characterScale;
+	Vector2 movement;
+	public string previousState;
+
+	// Äëÿ AStar-Path
+	private AIPath aiPath;
+	private AIDestinationSetter aiTarget;
+
+	public Transform searchPoint;
+	public float searchDistance = 12f;
+
+	public Transform attackPoint;
+	public float attackDistance = 2f;
+
+	Transform enemyTransform;
+
+	public override int MaxHP { get; set; }
+	public override int CurrentHP { get; set; }
+	public override bool IsDie { get; set; }
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		rigidbody = GetComponent<Rigidbody2D>();
+		characterScale = transform.localScale;
+		currentState = "Idle";
+		SetCharacterState(currentState);
+		aiPath = GetComponent<AIPath>();
+		aiTarget = GetComponent<AIDestinationSetter>();
+		enemyTransform = GetComponent<Transform>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		InvokeRepeating("SearchPath", 1f, .5f);
+		if (aiPath.desiredVelocity.x >= .01f)
+			enemyTransform.localScale = new Vector3(2f, 2f, 2f);
+		else if (aiPath.desiredVelocity.x <= -.01f)
+			enemyTransform.localScale = new Vector3(-2f, 2f, 2f);
+	}
+
+	void SearchPath()
+	{
+		SearchPathToPlayer(aiTarget, searchPoint, searchDistance);
+	}
+
+	#region Animation
+	public void SetCharacterState(string state)
+	{
+
+		if (state.Equals("Run"))
+		{
+			SetAnimation(run, true, 2f);
+		}
+
+		else
+		{
+			SetAnimation(idle, true, 1f);
+		}
+
+		currentState = state;
+	}
+
+	public void SetAnimation(AnimationReferenceAsset animation, bool loop, float timeScale)
+	{
+		if (animation.name.Equals(currentAnimation))
+		{
+			return;
+		}
+		Spine.TrackEntry animationEntry = skeletonAnimation.state.SetAnimation(0, animation, loop);
+		animationEntry.TimeScale = timeScale;
+		animationEntry.Complete += AnimationEntry_Complete;
+		currentAnimation = animation.name;
+	}
+
+	private void AnimationEntry_Complete(Spine.TrackEntry trackEntry)
+	{
+		if (currentState.Equals("Dash"))
+		{
+			SetCharacterState(previousState);
+		}
+	}
+	#endregion
+
+	public override void TakeDamage(int damage) 
+	{
+		
+	}
+	public override void Attack(Collider2D player) { }
+	public override void Die() { }
+
+	private void OnDrawGizmosSelected()
+	{
+		if (searchPoint != null)
+			Gizmos.DrawWireSphere(searchPoint.position, searchDistance);
+
+		if (attackPoint != null)
+			Gizmos.DrawWireSphere(attackPoint.position, attackDistance);
+	}
+}
---
+++ Assets/Skripts/Enemy/Enemy.cs
@@ -0,0 +1,38 @@
+using Pathfinding;
+using UnityEngine;
+
+public abstract class Enemy : MonoBehaviour
+{
+	public abstract int MaxHP { get; set; }
+	public abstract int CurrentHP { get; set; }
+	public abstract bool IsDie { get; set; }
+	public float Damage;
+
+	public abstract void TakeDamage(int damage);
+	public abstract void Attack(Collider2D player);
+	public abstract void Die();
+
+	Transform enemyTransform;
+	SpriteRenderer enemySprite;
+
+
+	private void Update()
+	{}
+
+	private void Start()
+	{
+		enemyTransform = GetComponent<Transform>();
+		enemySprite = GetComponent<SpriteRenderer>();
+	}
+
+	// Search and go to Player
+	public void SearchPathToPlayer(AIDestinationSetter ai, Transform searchPoint, float radius)
+	{
+		Collider2D player = Physics2D.OverlapCircle(searchPoint.position, radius, LayerMask.GetMask("Player"));
+
+		if (player != null && ai.target == null)
+			ai.target = GameObject.FindGameObjectWithTag("Player").transform;
+		else if (player == null)
+			ai.target = null;
+	}
+}
